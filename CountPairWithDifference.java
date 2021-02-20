59bc50ce0fbd1aed95730cc17be26130e0824353
Mohd Arif <mohdarif8299@gmail.com>
2021-02-20 13:59:04 +0530

    Count Pair with Difference (Two Pointer Approach)

--- CountPairWithDifference.java
+++ CountPairWithDifference.java
@@ -1,24 +1,29 @@
 import java.util.*;
 public class CountPairWithDifference {
     public static void main(String[] args) {
-        int A[] = {1,1,1,2,2};
+        int A[] = {1,1};
         int B=0;
         System.out.println(solve(A,B));
     }
     public static int solve(int A[],int B) {
-       int count=0,p1=0,p2=1;
+       int count=0,P1=0,P2=1;
        Arrays.sort(A);
-       while(p2<A.length) {
-        if(A[p2]-A[p1]==B) {
-                count++;
-               p2++;
-               p1++;
+       System.out.println(Arrays.toString(A));
+       while(P2<A.length) {
+        while(P1<P2-1 && A[P1]==A[P1+1]) {
+            P1++;
+        }
+        while(P2<A.length-1 && A[P2]==A[P2+1]) {
+            P2++;
+        }
+        System.out.println(P1+"   "+P2);
+         if(A[P2]-A[P1]==B && P1!=P2) {
+               count++;
+               P2++;
+               P1++;
         }   
-           else if(A[p2]-A[p1]>B) {
-               p1++;
-           }
-         
-           else p2++;
+        else if(A[P2]-A[P1]>B)   P1++;
+         else P2++;
        }
 
        return count;
