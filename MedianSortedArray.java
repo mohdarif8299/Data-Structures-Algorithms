db0ccdb9a91ce09049f724094c1f422e0302c1d8
Mohd Arif <mohdarif8299@gmail.com>
2021-03-07 11:16:20 +0530

    Median Of Two Sorted Arrays (Binray Search)

--- MedianSortedArray.java
+++ MedianSortedArray.java
@@ -1,37 +1,43 @@
 import java.util.*;
 public class MedianSortedArray {
     public static void main(String[] args) {
-        Integer AR[] = {1,4,5 };
+        Integer AR[] = {2, 3, 5, 8};
         ArrayList<Integer> A = new ArrayList<>(Arrays.asList(AR));
-        Integer BR[] ={2,3};
+        Integer BR[] ={10, 12, 14, 16, 18, 20};
         ArrayList<Integer> B = new ArrayList<>(Arrays.asList(BR));
         System.out.println(solve(A,B));
     }
     public static double solve(ArrayList<Integer> A,ArrayList<Integer> B) {
+        int K = (A.size()+B.size())/2;
+        double median = 0,ans=0;
+        median = count(A,B,K);
+        ans = count(A,B,K-1);
+        if((A.size()+B.size())%2==1) return median;
+        return (median+ans)/2;
+    }
+    public static int count(ArrayList<Integer> A,ArrayList<Integer> B,int K) {
         int low = Math.min(A.get(0),B.get(0));
         int high = Math.max(A.get(A.size()-1),B.get(B.size()-1));
-        double median = 0;
         while(low<=high) {
             int mid = (low+high)/2;
-            int X = (A.size()+B.size())/2;
-            long count = count(A,mid);
-            count+=count(B,mid);
-            if(count==X) {
-                median = mid;
-                low = mid+1;
+            int count = countLess(A,mid)+countLess(B,mid);
+            int countEqual = (countEqualLess(A,mid)-countLess(A,mid))+(countEqualLess(B,mid)-countLess(B,mid));
+            if(count>K) {
+                high =mid-1;
+            } else {
+                if(count+countEqual<=K) {
+                    low = mid+1;
+                }
+                else {
+                    return mid;
+                }
             }
-            else if(count<X) {
-                low = mid+1;
-            }
-            else {
-                high = mid-1;
-            }
-        }
-        return median;
+        }  
+        return 0;
     }
-    public static long count(ArrayList<Integer> A,int midElement) {
+    public static int countLess(ArrayList<Integer> A,int midElement) {
         int low = 0,high = A.size()-1;
-        long elementsCount = 0;
+        int elementsCount = 0;
         while(low<=high) {
             int mid = (low+high)/2;
             if(A.get(mid)<midElement) {
@@ -40,6 +46,21 @@ public class MedianSortedArray {
             }
             else {
                 high = mid-1;
+            }
+        }
+        return elementsCount;
+    }
+    public static int countEqualLess(ArrayList<Integer> A,int midElement) {
+        int low = 0,high = A.size()-1;
+        int elementsCount = 0;
+        while(low<=high) {
+            int mid = (low+high)/2;
+            if(A.get(mid)<=midElement) {
+                elementsCount = mid+1;
+                low = mid+1;
+            }
+            else {
+                high = mid-1;   
             }
         }
         return elementsCount;
