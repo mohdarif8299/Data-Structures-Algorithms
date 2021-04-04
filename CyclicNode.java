a15dbeb81b08d5392070fb8b8ba56d84e7e2980b
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2021-04-04 17:19:57 +0530

    Cyclic Node in Linked List

---
+++ CyclicNode.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+          if(head==null || head.next==null) return null;
+        ListNode p1 = head;
+        ListNode p2 = head;
+        int falg = 0;
+        while(true ) {
+           if(p2.next==null || p2.next.next==null) {
+                return null;   
+           }
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if(p1==p2) {
+                break;
+            }
+        }
+        p1 = head;
+        while(p1!=p2) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p1;
+    }
+}
