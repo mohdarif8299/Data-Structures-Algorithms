cb89306bf6a65559f680e3bc129f460f13a54281
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2021-04-11 11:42:57 +0530

    Height Of Binary Tree

---
+++ HeightOfBinaryTree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int solve(TreeNode A) {
+        if(A==null) return 0;
+        int leftHeight = solve(A.left);
+        int rightHeight = solve(A.right);
+        return Math.max(rightHeight,leftHeight)+1;
+    }
+}
