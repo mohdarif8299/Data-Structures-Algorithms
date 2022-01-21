151318c341514d0d7586baf4420f0bc596adddbd
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2022-01-21 23:32:59 +0530

    Time: 1 ms (17.99%), Space: 39 MB (33.69%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -2,11 +2,10 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length-1;
         int last = digits[n];
-        digits[n] = (digits[n] + 1) % 10;
-        int carry = (last + 1) / 10;
-        for(int i=n-1;i>=0;i--) {
+        int carry = 1;
+        for(int i=n;i>=0;i--) {
             last = digits[i];
-            digits[i] = (digits[i]+carry)%10;
+            digits[i] = (digits[i] + carry) % 10;
             carry = (last + carry) / 10;
         }
         
