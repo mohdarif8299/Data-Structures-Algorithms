ef006afc6b799d4e13324c7a2b2654725af3fe4a
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2022-02-06 13:26:26 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+       ArrayList<Integer> list = new ArrayList<>();
+       Queue<Integer> queue = new LinkedList<>();
+       boolean isVisited[] = new boolean[V+1];
+       queue.add(0);
+       isVisited[0] = true;
+       list.add(0);
+       while(!queue.isEmpty()) {
+           int source = queue.poll();
+           for(int node: adj.get(source)) {
+               if(isVisited[node] == false) {
+                   list.add(node);
+                   isVisited[node] = true;
+                   queue.add(node);
+               }
+           }
+       }
+       return list;
+     }
+}
