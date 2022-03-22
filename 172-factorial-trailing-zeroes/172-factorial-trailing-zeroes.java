0fcd93eef49f4276afef2f28b104ef6b46ef4311
Mohd Arif <31352655+mohdarif8299@users.noreply.github.com>
2022-03-22 11:31:32 +0530

    Time: 1 ms (84.32%), Space: 41.1 MB (52.28%) - LeetHub

---
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        int i = 5;
+        while(i <= n) {
+            count += n/i;
+            i *= 5;
+        }
+        return count;
+    }
+}
